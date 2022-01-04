c6c6c506b9cd3c6a7a656cff03cc401efe909a7e
Audrey Schmidt <audsch@outlook.com>
2022-01-04 13:42:22 -0600

    created AddEntryForm component that passes from child to parent and instantiated in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}]) //hardcoded first entry with array that contains one object; can leave empty list
+
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry]; // '...' spreads apart to prevent nested array; adds newEntry to END of existing entries
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
     </div>
   );
 }
